58496cad7bfd01cd7ab4c75a493dc8feb98bb1b5
Hugo Cabral <hugocabral2803@gmail.com>
2021-08-15 01:14:35 -0300

    Exercicio com Estrutura de repetição

---
+++ logica-de-programacao/exercicio-aula08/exercicio-3.js
@@ -0,0 +1,21 @@
+let nomesComuns = ["Miguel", "Laura", "Lucas", "Beatriz", "Guilherme", "Maria", "Gabriel", "Ana", "Arthur", "Júlia", 
+"Enzo", "Alice", "Rafael", "Mariana", "João", "Larissa", "Gustavo", "Maria Eduarda", "Pedro", "Sofia", 
+"Bernardo", "Isabela", "Matheus", "Helena", "Davi", "Camila", "Heitor", "Lara", "Henrique", "Valentina", 
+"Bruno", "Letícia", "Samuel", "Luana", "Felipe", "Amanda", "Lorenzo", "Yasmin", "Benjamin", "Sophia", 
+"Vinícius", "Rebeca", "Rodrigo", "Juliana", "Eduardo", "Bruna", "Diego", "Cecília", "Antônio", "Fernanda", 
+"Leonardo", "Isadora", "Noah", "Lorena", "Nícolas", "Lívia", "Daniel", "Manuela", "Thiago", "Vitória"];
+
+let nome = "Vitória";
+let eComum = false;
+
+for(i = 0; i < nomesComuns.length; i++){
+  if(nome == nomesComuns[i]){
+    eComum = true;
+    break;
+  }
+}
+if (eComum){
+  console.log("É nome comum :P");
+}else{
+  console.log("Diferentão, hein? XD");
+}
